f75d85d7667e294d9b5f27fb7929d286a4f2e0b3
Liberman, Tzipora (tl314x) <tl314x@intl.att.com>
2018-08-22 10:18:29 +0300

    fix components bugs

--- stories/angular/checklist.stories.ts
+++ stories/angular/checklist.stories.ts
@@ -1,11 +1,12 @@
 import { storiesOf } from '@storybook/angular';
-import { withKnobs, text, number, boolean, array, select, color, date, button, object } from '@storybook/addon-knobs';
+import { withKnobs, text, number, boolean, array, select, color, date, button } from '@storybook/addon-knobs';
 import { withNotes } from '@storybook/addon-notes';
 import { action, configureActions } from '@storybook/addon-actions';
 import { moduleMetadata } from '@storybook/angular';
-import { ChecklistComponent, CheckboxComponent } from '../../src/angular/components';
-import { ChecklistModel, ChecklistItemModel } from '../../src/angular/common';
-import { RippleAnimationAction } from '../../src/angular/animations/ripple-click.animation.directive';
+import { CheckboxComponent, ChecklistComponent } from '../../src/angular/components';
+import { RippleClickAnimationDirective } from '../../src/angular/animations/ripple-click.animation.directive';
+import { ChecklistModel } from '../../src/angular/checklist/models/Checklist';
+import { ChecklistItemModel } from '../../src/angular/checklist/models/ChecklistItem';
 
 const checkListModelExample: ChecklistModel =  new ChecklistModel([],[
   new ChecklistItemModel('apple'),
@@ -19,34 +20,34 @@ storiesOf('Form elements|Checklist', module)
   .addDecorator(
     moduleMetadata({
       declarations: [
-        ChecklistComponent,
-        RippleAnimationAction,
-        CheckboxComponent
+        CheckboxComponent,
+        RippleClickAnimationDirective,
+        ChecklistComponent
       ],
       imports: [
       ]
     })
   )
   .add('Simple checklist', () => {
-      //const _checklistModel = object('checklistModel', checkListModelExample);
-      const _checkedChange = text('*(checkedChange)', 'Event throws when checklist changed, see in Action logger tab.');
-      const _testId = text('testId', 'smalpe-test-id');
+        const _checkedChange = text('*(checkedChange)', 'Event throws when checklist changed, see in Action logger tab.');
+        const _testId = text('testId', 'check-list-test-id');
+
       return {
         props: {
             checkedChange: action('Checklist changed '),
-            checkListModelExample, _testId
+            _testId, checkListModelExample
         },
         template: `
         <sdc-checklist 
-          [checklistModel]="checkListModelExample"
-          [testId]="_testId"
-          (checkedChange)="checkedChange($event)">
-        </sdc-checklist>
+                [checklistModel]="checkListModelExample"
+                [testId]="_testId"
+                (checkedChange)="checkedChange($event)">
+            </sdc-checklist>
         `
       }
     },
-    { notes: `<h2>Checklist</h2>
-            Full example of checklist.
+    { notes: `<h2>Checkbox</h2>
+    Full example of checklist.
             Use the KNOBS tab to change values.`
     }
 )
