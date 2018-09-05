8e1c05dc78b25e37ab6175679ff36be99c9db575
onap-sdc <onap.sdc@gmail.com>
2018-09-05 09:59:54 +0300

    Update calculate-test-id.pipe.ts

--- src/angular/utils/calculate-test-id.pipe.ts
+++ src/angular/utils/calculate-test-id.pipe.ts
@@ -3,6 +3,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name: 'calculateTestId'})
 export class CalculateTestIdPipe implements PipeTransform {
     transform(value:string, containerTestId:string) : string {
+        if (!value) {
+          return "";
+        }
+        if (!containerTestId) {
+          return value.toLowerCase().split(' ').join('-');
+        }
         return containerTestId + '-' + value.toLowerCase().split(' ').join('-');
     }
 }
