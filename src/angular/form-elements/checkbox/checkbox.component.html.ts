7cb799e40ca48411504b49370c78df6dfb05edcc
onap-sdc <onap.sdc@gmail.com>
2018-09-12 13:10:56 +0300

    Update checkbox.component.html.ts

--- src/angular/form-elements/checkbox/checkbox.component.html.ts
+++ src/angular/form-elements/checkbox/checkbox.component.html.ts
@@ -1,8 +1,6 @@
 export const template = `
-<div class="sdc-checkbox">
-    <label SdcRippleClickAnimation [rippleClickDisabled]="disabled">
-        <input type="checkbox" class="sdc-checkbox__input" [ngModel]="checked" (ngModelChange)="toggleState($event)" [disabled]="disabled" [attr.data-tests-id]="testId">
-        <span class="sdc-checkbox__label" [attr.data-tests-id]="testId">{{ label }}</span>
-    </label>
-</div>
+<label SdcRippleClickAnimation [rippleClickDisabled]="disabled">
+  <input type="checkbox" class="sdc-checkbox__input" [ngModel]="checked" (ngModelChange)="toggleState($event)" [disabled]="disabled" [attr.data-tests-id]="testId">
+  <span class="sdc-checkbox__label" [attr.data-tests-id]="testId">{{ label }}</span>
+</label>
 `;
