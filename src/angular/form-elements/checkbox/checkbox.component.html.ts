6e08bbcd011a4ab3e8fb9ea69b675b1233476040
Liberman, Tzipora (tl314x) <tl314x@intl.att.com>
2018-09-12 11:10:34 +0300

    Move checkbox test id from input to span.

--- src/angular/form-elements/checkbox/checkbox.component.html.ts
+++ src/angular/form-elements/checkbox/checkbox.component.html.ts
@@ -2,7 +2,7 @@ export const template = `
 <div class="sdc-checkbox">
     <label SdcRippleClickAnimation [rippleClickDisabled]="disabled">
         <input type="checkbox" class="sdc-checkbox__input" [ngModel]="checked" (ngModelChange)="toggleState($event)" [disabled]="disabled" [attr.data-tests-id]="testId">
-        <span class="sdc-checkbox__label">{{ label }}</span>
+        <span class="sdc-checkbox__label" [attr.data-tests-id]="testId">{{ label }}</span>
     </label>
 </div>
 `;
