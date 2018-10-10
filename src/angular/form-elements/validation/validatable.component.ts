c4bf7fd86e4b6654868e4c22979812d8d0d3b31e
Avivi, Yotam (ya107f) <ya107f@intl.att.com>
2018-10-10 15:02:14 +0300

    Fix input being red when no validation is taking place on various inputs

--- src/angular/form-elements/validation/validatable.component.ts
+++ src/angular/form-elements/validation/validatable.component.ts
@@ -6,20 +6,28 @@ export abstract class ValidatableComponent implements IValidatableComponent {
     // Each ValidatableComponent should handle the style in case of error, according to this boolean
     public valid: boolean;
     public dirty: boolean;
+    public validationActive: boolean;
+
     // Each ValidatableComponent will notify when the value is changed.
     public notifier: Subject<string>;
 
     constructor() {
         this.notifier = new Subject();
         this.dirty = false;
+        this.validationActive = false;   //This flag will be set to true by the ValidationComponent if validation should be applied
     }
 
     public abstract getValue(): any;
 
     // Each ValidatableComponent should call the valueChanged on value changed function.
     protected valueChanged = (value: any): void => {
+
+      //If validation on this component is activated by the ValidationComponent - do the validation
+      if (this.validationActive) {
         this.dirty = true;
         this.notifier.next(value);
+      }
+
     }
 
 }
