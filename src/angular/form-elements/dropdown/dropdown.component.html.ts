bdf862d7e7fc19e4f0fc0f0951306be33cc611ae
Avivi, Yotam (ya107f) <ya107f@intl.att.com>
2018-12-25 23:40:30 +0200

    fixed dropdown pre-selected issue

--- src/angular/form-elements/dropdown/dropdown.component.html.ts
+++ src/angular/form-elements/dropdown/dropdown.component.html.ts
@@ -2,7 +2,7 @@ export const template = `
 <div (clickOutside)="closeListOptions()" class="sdc-dropdown" [attr.data-tests-id]="testId">
       <sdc-input class="sdc-dropdown__input js-sdc-dropdown--toggle-hook"
               [disabled]="disabled"
-              [value]="this.filterValue"
+              [value]="this.selectedOption.value"
               [required]="required"
               [size]="size"
               [label]="label"
