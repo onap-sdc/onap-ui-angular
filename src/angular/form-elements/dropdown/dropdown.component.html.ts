6356859e0c6a0019c2fa507afa970fadea67c894
onap-sdc <onap.sdc@gmail.com>
2019-05-16 12:54:33 +0300

    Fix clickOutside
    Allow clickedOutside to be triggered when clicking on one dropdown after another. Removing unused js-dropdown-toggle hook

--- src/angular/form-elements/dropdown/dropdown.component.html.ts
+++ src/angular/form-elements/dropdown/dropdown.component.html.ts
@@ -1,7 +1,7 @@
 export const template = `
 <div (clickOutside)="closeListOptions()" class="sdc-dropdown" [attr.data-tests-id]="testId">
       <input type="hidden" class="sdc-dropdown__value" [name]="name" [value]="this.selectedOption.value" />
-      <sdc-input class="sdc-dropdown__input js-sdc-dropdown--toggle-hook" #dropdownInput
+      <sdc-input class="sdc-dropdown__input" #dropdownInput
               [disabled]="disabled"
               [value]="this.selectedOption.label"
               [required]="required"
@@ -11,7 +11,7 @@ export const template = `
               [label]="label"
               [isIconClickable]="!disabled"
               [righIconName]="'caret1-down-o'"
-              (click)="toggleDropdown($event)"
+              (click)="toggleDropdown()"
               [placeHolder]="this.selectedOption?.label || this.selectedOption?.value || placeHolder">
       </sdc-input>
       <dropdown-results *ngIf="show" [options]="allOptions" [isGroupDesign]="isGroupDesign" [selectedOption]="selectedOption" (onItemSelected)="selectOption($event)"></dropdown-results>
