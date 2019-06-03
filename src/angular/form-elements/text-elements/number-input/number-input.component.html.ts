52f30353301ad16af4714c481fc4b6a67d3be181
Gordon, Nechama (ng689e) <ng689e@intl.att.com>
2019-06-03 14:01:30 +0300

    Adding sdc-number-input component with spinner and automatic limiting of input value

---
+++ src/angular/form-elements/text-elements/number-input/number-input.component.html.ts
@@ -0,0 +1,26 @@
+export const template = `
+<div class="sdc-input sdc-number-input">
+    <label class="sdc-input__label" *ngIf="label" [ngClass]="{'required':required}">{{label}}</label>
+    <div class="sdc-input-wrapper">
+        <input
+            class="sdc-input__input sdc-input__number-input {{classNames}} {{size}}"
+            [ngClass]="{'error': (!valid && dirty), 'disabled':disabled}"
+            [attr.name]="name ? name : null"
+            [placeholder]="placeHolder"
+            [(ngModel)]="value"
+            [max]="maxValue"
+            [min]="minValue"
+            [type]="'number'"
+            (keyup)="validateMax($event.target.value)"
+            (blur)="validateMin($event.target.value)"
+            [formControl]="control"
+            [attr.disabled]="(disabled || isViewMode) ? 'disabled' : null"
+            [attr.data-tests-id]="testId"
+        />
+        <div class="sdc-input__spinner-buttons" *ngIf="!(disabled || isViewMode)">
+            <svg-icon name="caret1-up-o" mode="secondary" (click)="clickUp()" [clickable]="true" size="small" [testId]="testId +'-up-icon'"></svg-icon>
+            <svg-icon name="caret1-down-o" mode="secondary" (click)="clickDown()" [clickable]="true" size="small" [testId]="testId +'-down-icon'"></svg-icon>
+        </div>
+    </div>
+</div>
+`;
